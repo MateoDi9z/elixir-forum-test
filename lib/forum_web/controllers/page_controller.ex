f837aba91b1e20d783f8d46c8858e10b3d6a4e8e
MateoDi9z <dzmateozd@gmail.com>
2024-01-28 00:34:32 -0300

    posts

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,16 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "John Doe", email: "jhon@gmail.com"},
+      %{id: 2, name: "Bob Doe", email: "bob@gmail.com"},
+      %{id: 3, name: "Alice Doe", email: "alice@gmail.com"},
+    ]
+
+    # render(conn, :users, users: users, layout: false) - HTML
+
+    json(conn, %{users: users}) # - JSON
+  end
 end
